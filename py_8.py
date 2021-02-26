ab2c22dc0ed06f69399264b27f274910ea8e03c5
sava <savadyagilev@yandex.ru>
2021-02-26 17:55:30 +0300

    No 3

--- py_8.py
+++ py_8.py
@@ -4,9 +4,9 @@ from flask import Flask, render_template
 app = Flask(__name__)
 
 
-@app.route('/training/<prof>')
-def training(prof):
-    return render_template('text.html', prof=prof)
+@app.route('/list_prof/<list>')
+def list_prof(list):
+    return render_template('text.html', list=list)
 
 if __name__ == '__main__':
     app.run(port=8080, host='127.0.0.1')
