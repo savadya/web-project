0d4e1f2240df4a53c02b58c5a989f74b3b9b9985
sava <savadyagilev@yandex.ru>
2021-03-01 18:21:24 +0300

    no 3 fix

--- py_8.py
+++ py_8.py
@@ -7,7 +7,8 @@ app = Flask(__name__)
 @app.route('/<title>')
 @app.route('/index/<title>')
 def index(title):
-    return render_template('text.html', title=title)
+    prof = ['инженер-исследователь', 'пилот', 'строитель', 'экзобиолог', 'врач', 'инженер по терраформированию', 'климатолог', 'специалист по радиационной защите', 'астрогеолог', 'гляциолог', 'инженер жизнеобеспечения', 'метеоролог', 'оператор марсохода', 'киберинженер', 'штурман']
+    return render_template('text.html', title=title, prof=prof)
 
 if __name__ == '__main__':
     app.run(port=8080, host='127.0.0.1')
